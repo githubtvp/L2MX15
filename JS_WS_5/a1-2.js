e6efa6abf1f6884e8819f2d72deeecc315ab668e
githubtvp <tvp1640@gmail.com>
2023-12-05 07:12:11 +0530

    Updates from L2

--- JS_WS_5/a1-2.js
+++ JS_WS_5/a1-2.js
@@ -1,3 +1,4 @@
+
 const prompt = require('prompt-sync')();
 
 const arr = [0, 1, 2, 3, 4, 5, 6];
@@ -28,6 +29,8 @@ function getArrEl() {
   return numbersArray;
 }
 
+const posArr = arr.filter((num)=>{ return num > 0});
+prArr(posArr);
 //const numArr;
 let numArr = [];
 function qsn1() {
@@ -52,12 +55,92 @@ function qsn2() {
 
   pr("Max no : " + maxNo(numArr));
 }
+
+function sumOfDigits(number, splitCh) {
+  // Convert the number to a string to iterate over its digits
+  const digits = number.toString().split(splitCh).map(Number);
+
+  // Use the reduce method to sum up the digits
+  const digitSum = digits.reduce((acc, digit) => acc + digit, 0);
+  //pr(digitSum);
+  return digitSum;
+}
+
+// Read a number from the user
+function qsn3() {
+  const userInput = prompt("Enter a integer value :");
+
+  // Convert the user input to a number
+  const userNumber = parseInt(userInput);
+
+  // Check if the conversion was successful
+  if (!isNaN(userNumber)) {
+      // Call the function to get the sum of digits
+      const result = sumOfDigits(userNumber, '');
+
+      // console.log(`Sum of digits of ${userNumber}:`, result);
+      pr(`Sum of digits of ${userNumber} : ${result} `);
+  } else {
+      alert("Invalid input. Please enter a valid number.");
+  }
+}
+
+function qsn4() {
+  let str = prompt("Enter a string : ");
+  pr("You entered : \n" + str);
+  let arrStr = [];
+  //arrStr = str.split(/\s+/);
+  //arrStr = arrStr.reverse();
+  str = str.trim();
+  // Iterate over the string from the end to the beginning
+  for (let i = str.length - 1; i >= 0; i--) {
+      arrStr.push(str[i]);
+  }
+  prArr(arrStr);
+  while (arrStr.length > 0)
+      process.stdout.write(arrStr.pop());
+}
+
+
+// Read a number from the user
+function qsn6() {
+  const userInput = prompt("Enter integer numbers separated by commas :");
+
+  // Convert the number to a string to iterate over its digits
+  let digits = [];
+  digits = userInput.toString().split(',').map(numStr => parseInt(numStr.trim()));
+
+  // Use the reduce method to sum up the digits
+  const sum = digits.reduce((acc, digit) => acc + digit, 0);
+  //pr(digitSum);
+
+  // console.log(`Sum of digits of ${userNumber}:`, result);
+  pr(`Sum of digits of ${userInput} : ${sum} `);
+
+}
+
 ln();
 ln(1);
 qsn1();
 ln(2);
 qsn2();
 ln();
+ln(3);
+qsn3();
+ln();
+ln(4);
+qsn4();
+ln();
+ln(6);
+qsn6();
+
+
+
+
+
+
+
+
 function ln(n) {
   for (i = 0; i < 25; i++) {
     process.stdout.write('-');
---
+++ JS_WS_5/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+// Using filter to get even numbers
+// const evenNumbers = numbers.filter(function(number) {
+//   return number % 2 === 0;
+// });
